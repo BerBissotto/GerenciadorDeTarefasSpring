79b50ec79ace5aae557eb332c7cd1d07408d5a9c
Berzinsk <bissotto.bernardo@gmail.com>
2026-06-24 15:24:48 -0300

    Primeiro commit, criação do projeto com configuração do application.properties

---
+++ src/main/java/com/example/GerenciadorDeTarefasSpring/GerenciadorDeTarefasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.GerenciadorDeTarefasSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTarefasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GerenciadorDeTarefasSpring/GerenciadorDeTarefasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GerenciadorDeTarefasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
